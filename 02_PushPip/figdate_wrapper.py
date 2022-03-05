6f34b2ae1ee7d7381f8d7def7e8f890a744c9589
BondarevIvan <bondarevivan2001@mail.ru>
2022-03-06 00:51:08 +0300

    add figdate wrapper

---
+++ 02_PushPip/figdate_wrapper.py
@@ -0,0 +1,22 @@
+import venv, sys, shutil
+from subprocess import run
+
+
+def create_venv(directory: str):
+    venv.create(directory, with_pip=True)
+
+
+def install_figlet(venv_directory: str):
+    run([f'{venv_directory}/bin/pip', 'install', 'pyfiglet'])
+
+
+def run_figdate(venv_directory: str, args: list):
+    run([f'{venv_directory}/bin/python3', '-m', 'figdate'] + args)
+
+
+if __name__ == '__main__':
+    venv_dir = '/tmp/venv_02_pushpip'
+    create_venv(venv_dir)
+    install_figlet(venv_dir)
+    run_figdate(venv_dir, sys.argv[1:])
+    shutil.rmtree(venv_dir)
